bdf5e5ee64cc8b820e952ae279d6533c238ef74f
Yaswanth-Mostedge <Yaswanth.Vinnakota@mostedge.com>
2026-08-26 05:55:22 -0400

    now boot without a datasource; any DB-backed endpoints will fail at request time until a real DB is available

--- src/main/java/com/commercex/CommerceXApplication.java
+++ src/main/java/com/commercex/CommerceXApplication.java
@@ -2,8 +2,13 @@ package com.commercex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		HibernateJpaAutoConfiguration.class,
+		DataSourceAutoConfiguration.class
+})
 public class CommerceXApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CommerceXApplication.class, args);
