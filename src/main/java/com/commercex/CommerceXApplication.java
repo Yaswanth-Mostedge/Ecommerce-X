ae5393a20c60afb7713afc150714a5880bfcad1d
Yaswanth-Mostedge <Yaswanth.Vinnakota@mostedge.com>
2026-08-27 07:53:41 -0400

    fix code

--- src/main/java/com/commercex/CommerceXApplication.java
+++ src/main/java/com/commercex/CommerceXApplication.java
@@ -2,13 +2,8 @@ package com.commercex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		HibernateJpaAutoConfiguration.class,
-		DataSourceAutoConfiguration.class
-})
+@SpringBootApplication
 public class CommerceXApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CommerceXApplication.class, args);
